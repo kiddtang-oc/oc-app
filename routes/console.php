9526bcf46cc24678deafad5058ad3cf9b0a4e1ca
Kidd Tang <kidd.tang@ocglobaltech.com>
2026-04-03 13:32:20 +0800

    Customize Docker Image for Cron

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $time = now()->format('Y-m-d H:i:s');
+    Log::info("Hello Kidd, the time now is: {$time}");
+})->everyMinute();
